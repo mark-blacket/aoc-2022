16fd3ffe4e50325f5610207b5929f507991ef6c5
mark_blacket <51699458+mark-blacket@users.noreply.github.com>
2022-12-05 14:19:29 +0300

    days 4 and 5

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,58 @@ fn day3_2() -> io::Result<()> {
     Ok(())
 }
 
+fn day4() -> io::Result<()> {
+    let f = BufReader::new(File::open("data/4.txt")?);
+    let mut total1 = 0;
+    let mut total2 = 0;
+
+    for line in f.lines() {
+        let v: Vec<u64> = line?.split(&['-', ','][..]).map(|x| x.parse().unwrap()).collect();
+        if (v[0] >= v[2] && v[1] <= v[3]) || (v[0] <= v[2] && v[1] >= v[3]) {
+            total1 += 1;
+        }
+        if !(v[0] > v[3] || v[2] > v[1]) {
+            total2 += 1;
+        }
+    }
+
+    println!("4-1: {}", total1);
+    println!("4-2: {}", total2);
+    Ok(())
+}
+
+fn day5(task: u32) -> io::Result<()> {
+    let mut input = vec![
+        vec![],
+        vec!['s', 't', 'h', 'f', 'w', 'r'],
+        vec!['s', 'g', 'd', 'q', 'w'],
+        vec!['b', 't', 'w'],
+        vec!['d', 'r', 'w', 't', 'n', 'q', 'z', 'j'],
+        vec!['f', 'b', 'h', 'g', 'l', 'v', 't', 'z'],
+        vec!['l', 'p', 't', 'c', 'v', 'b', 's', 'g'],
+        vec!['z', 'b', 'r', 't', 'w', 'g', 'p'],
+        vec!['n', 'g', 'm', 't', 'c', 'j', 'r'],
+        vec!['l', 'g', 'b', 'w']
+    ];
+    let f = BufReader::new(File::open("data/5.txt")?);
+
+    for line in f.lines() {
+        let v: Vec<usize> = line?.split(' ').skip(1).step_by(2)
+            .map(|x| x.parse().unwrap()).collect();
+        let l = input[v[1]].len() - v[0];
+        let mut moved = input[v[1]].split_off(l);
+        if task == 1 {
+            moved.reverse();
+        }
+        input[v[2]].append(&mut moved);
+    }
+
+    // println!("{:?}", input);
+    let s: String = input[1..].iter_mut().map(|x| x.pop().unwrap()).collect();
+    println!("5-{}: {}", task, s.to_uppercase());
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     print!("{}", "Enter day number: ");
@@ -87,6 +139,8 @@ fn main() -> io::Result<()> {
         1 => day1(),
         2 => day2(),
         3 => { day3_1()?; day3_2() },
+        4 => day4(),
+        5 => { day5(1)?; day5(2) },
         _ => {
             println!("Incorrect day number: {}", input.as_str());
             Ok(())
