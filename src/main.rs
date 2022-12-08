ec3cfa51d1bcb542400952c90705a209a498fe79
mark_blacket <51699458+mark-blacket@users.noreply.github.com>
2022-12-08 05:12:06 +0300

    day 7

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
+use std::collections::HashMap;
 
 fn day1() -> io::Result<()> {
     let f = BufReader::new(File::open("data/1.txt")?);
@@ -149,6 +150,44 @@ fn day6(size: usize, task: u32) -> io::Result<()> {
     Ok(())
 }
 
+fn day7() -> io::Result<()> {
+    let f = BufReader::new(File::open("data/7.txt")?);
+    let mut sizes = HashMap::new();
+    let mut path = Vec::new();
+
+    for line in f.lines() {
+        let line = line?;
+        let v: Vec<&str> = line.split(' ').collect();
+        match v[0] {
+            "$"   => if v[1] == "cd" {
+                match v[2] {
+                    ".." => { path.pop(); },
+                    x    => { path.push(x.to_owned()); },
+                }
+            },
+            "dir" => (),
+            x     => {
+                let n = x.parse::<u64>().unwrap();
+                for i in 0..path.len() {
+                    let dir = path[..=i].join(" ");
+                    let size = match sizes.get(&dir) {
+                        Some(x) => x + n,
+                        None    => n
+                    };
+                    sizes.insert(dir, size);
+                }
+            }
+        }
+    }
+
+    let s: u64 = sizes.values().filter(|&&x| x <= 100000).sum();
+    let needed = 30000000 - (70000000 - sizes["/"]);
+    let min = sizes.values().filter(|&&x| x >= needed).min().unwrap();
+    println!("7-1: {}", s);
+    println!("7-2: {}", min);
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     print!("{}", "Enter day number: ");
@@ -161,6 +200,7 @@ fn main() -> io::Result<()> {
         4 => day4(),
         5 => { day5(1)?; day5(2) },
         6 => { day6(4, 1)?; day6(14, 2) },
+        7 => day7(),
         _ => {
             println!("Incorrect day number: {}", input.as_str());
             Ok(())
